a4dd03f887f2c7360a09bc93ea44416c653a08c6
Kotisheu <iiacherry@aim.com>
2014-05-02 02:15:42 -0400

    Create OccupantInCol.java

---
+++ OccupantInCol.java
@@ -0,0 +1,27 @@
+public class OccupantInCol 
+{ 
+ private Object occupant; 
+ private int col; 
+ 
+ public OccupantInCol(Object p, int c) 
+ { 
+ occupant = p; 
+ col = c; 
+ } 
+ 
+ public Object getOccupant() 
+ { 
+ return occupant; 
+ } 
+ 
+ public int getCol() 
+ { 
+ return col; 
+ } 
+ 
+ public void setOccupant(Object p) 
+ { 
+ occupant = p; 
+ } 
+ 
+}
